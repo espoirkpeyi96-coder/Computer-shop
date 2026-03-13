40cf2b82b15c97ef82daaef253b221098709128b
Espoir Kpeyi <espoirkpeyi96@gmail.com>
2026-03-13 11:40:11 +0100

    Mise à jour

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector("#menuToggle");
+const menuOptions = document.querySelector(".menu-options");
+  menuToggle.addEventListener("click", () => {
+  menuOptions.classList.toggle("active");
+});
+
+const button =document.querySelector(button);
+// button.style.color="red";
+button.addEventListener("onclick",()=>{
+  alert("Boutton activé");
+  
+});
